1a1390ce1448cd38cfb36c9a57cc915c87140beb
at-quando <quan.do@asiantech.vn>
2018-02-25 04:03:13 +0700

    1235

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '18.222.69.48', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
