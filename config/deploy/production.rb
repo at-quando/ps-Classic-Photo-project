9dae84a92d820db55e96458bb109f4c8fd784e18
at-quando <quan.do@asiantech.vn>
2018-02-25 12:00:11 +0700

    final

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,6 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '18.222.69.48', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
@@ -59,3 +58,8 @@ server '18.222.69.48', user: 'deploy', roles: %w{web app db}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+set :stage, :production
+set :rails_env, :production
+set :deploy_to, "/deploy/apps/classic_photo"
+set :branch, :config_deploy
+server "18.222.69.48", user: "deploy", roles: %w(web app db)
