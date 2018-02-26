fefcaa94dc07ccd647279f08cd653fda88b989e3
at-quando <quan.do@asiantech.vn>
2018-02-26 10:19:09 +0700

    final

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+lock "3.10.0"
+
 set :application, "classic_photo"
 set :repo_url, "git@github.com:at-quando/ps-Classic-Photo-project.git"
 set :pty, true
@@ -5,9 +7,6 @@ set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
-
-
-set :rvm_ruby_version, 'ruby-2.4.0'
 
 set :puma_rackup, -> {File.join(current_path, "config.ru")}
 set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
@@ -23,4 +22,3 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-set :puma_restart_command, 'bundle exec puma'
