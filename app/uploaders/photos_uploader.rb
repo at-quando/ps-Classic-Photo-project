81eeac97bb3a56d9b58851a2d93b09ba00b95f8d
at-quando <quan.do@asiantech.vn>
2018-03-14 11:50:13 +0700

    fix size

--- app/uploaders/photos_uploader.rb
+++ app/uploaders/photos_uploader.rb
@@ -6,7 +6,6 @@ class PhotosUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process :resize_to_limit => [600, nil]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
