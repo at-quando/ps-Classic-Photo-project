59d810ab362574ec31701a71b84725bcd3d95ca0
at-quando <quan.do@asiantech.vn>
2018-03-15 11:48:56 +0700

    ccontract

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,12 @@
 class ContractsController < ApplicationController
+  def show
+    @contract = Contract.find(params[:id])
+    ids = @contract.package.split(',')
+    @package = []
+    ids.each do |id|
+      pack = Package.find(id)
+      @package.push(pack) if pack 
+    end
+    @ctr_photo = ContractPhotographer.where(contract_id: params[:id])
+  end
 end
--- app/models/contract_photographer.rb
+++ app/models/contract_photographer.rb
@@ -1,2 +1,4 @@
 class ContractPhotographer < ApplicationRecord
+  belongs_to :contract
+  belongs_to :photographer
 end
