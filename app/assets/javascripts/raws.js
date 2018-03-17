87407507783ac41a6478e7e46126264d25c1956c
at-quando <quan.do@asiantech.vn>
2018-03-17 23:08:42 +0700

    vv4.2

---
+++ app/assets/javascripts/raws.js
@@ -0,0 +1,8 @@
+//=require raws/jquery.min.js
+//=require raws/jquery.poptrox.min.js
+//=require raws/jquery.scrolly.min.js
+//=require raws/jquery.scrollex.min.js
+//=require raws/skel.min.js
+//=require raws/util.js
+//=require raws/ie/respond.min.js
+//=require raws/main.js
