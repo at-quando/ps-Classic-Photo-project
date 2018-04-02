a0351b1019bb7d4d0a83b4936afcb4f15334cbbb
at-quando <quan.do@asiantech.vn>
2018-04-02 17:40:20 +0700

    123

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,6 +3,7 @@ class Contract < ApplicationRecord
   has_many :viewers
   has_many :carts
   has_many :plus
+  accepts_nested_attributes_for :plus, :allow_destroy => true
   accepts_nested_attributes_for :contract_plans, :allow_destroy => true
   accepts_nested_attributes_for :carts, :allow_destroy => true
   accepts_nested_attributes_for :viewers, :allow_destroy => true
