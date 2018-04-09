83b58ca64213563aa4c39ef4f3ae511cb9022bba
at-quando <quan.do@asiantech.vn>
2018-04-09 16:31:19 +0700

    13

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,19 +5,19 @@ class ArticlesController < ApplicationController
     if request.path == '/posing'
       @articles = Article.all.where(kind: 1)
       @article = Article.find_by(kind: 1, favorite: 1)
-      @articles_rand= Article.order("RAND()")
+      @articles_rand= Article.order("RAND()").limit(6)
     elsif request.path == '/clothes'
       @articles = Article.all.where(kind: 0)
       @article = Article.find_by(kind: 0, favorite: 1)
-      @articles_rand = Article.order("RAND()")
+      @articles_rand = Article.order("RAND()").limit(6)
     elsif request.path == '/ask'
       @articles = Article.all.where(kind: 3)
       @article = Article.find_by(kind: 3, favorite: 1)
-      @articles_rand= Article.order("RAND()")
+      @articles_rand= Article.order("RAND()").limit(6)
     elsif request.path == '/concept'
       @articles = Article.all.where(kind: 2)
       @article = Article.find_by(kind: 2, favorite: 1)
-      @articles_rand= Article.order("RAND()")
+      @articles_rand= Article.order("RAND()").limit(6)
     else 
       @articles = Article.all
     end
