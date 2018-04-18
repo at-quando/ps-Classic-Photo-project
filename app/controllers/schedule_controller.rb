e7d0782d6ec4163c906e3c2ef4d70b7d0dde871f
at-quando <quan.do@asiantech.vn>
2018-04-18 14:50:26 +0700

    123

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,5 +1,5 @@
 class ScheduleController < ApplicationController
   def index
-    @contracts = Contract.all.group_by(&:taken_date)
+    @contracts = Contract.all.order(taken_date: :desc).group_by(&:taken_date)
   end
 end
