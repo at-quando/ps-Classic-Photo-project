151e6543e9aa09c9a4c9276ad66f1faf5668cc5f
at-quando <quan.do@asiantech.vn>
2018-05-21 14:26:10 +0700

    pphotoshp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511072848) do
+ActiveRecord::Schema.define(version: 20180521071916) do
 
   create_table "accessories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20180511072848) do
     t.string "sign"
     t.date "taken_date_2"
     t.boolean "paid", default: false
+    t.boolean "photoshop"
     t.index ["price_id"], name: "index_contracts_on_price_id"
   end
 
